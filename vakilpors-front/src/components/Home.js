7f16ff4dbf1025c40cbbfeb19f9bcd833a4a72c7
AmirReza25 <118440255+AmirReza25@users.noreply.github.com>
2023-04-03 21:10:30 +0330

    Create basics of showing threads
    There isn't any css file writed yet

---
+++ vakilpors-front/src/components/Home.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+
+
+const Home = () => {
+    const [thread, setThread] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log({ thread });
+        setThread("");
+    };
+    return (
+        <>
+            <Nav />
+            <main className='home'>
+                <h2 className='homeTitle'>Create a Thread</h2>
+                <form className='homeForm' onSubmit={handleSubmit}>
+                    <div className='home__container'>
+                        <label htmlFor='thread'>Title / Description</label>
+                        <input
+                            type='text'
+                            name='thread'
+                            required
+                            value={thread}
+                            onChange={(e) => setThread(e.target.value)}
+                        />
+                    </div>
+                    <button className='homeBtn'>CREATE THREAD</button>
+                </form>
+            </main>
+        </>
+    );
+};
