063fa262a49d7ccecae1ce2f98e51feed49d81da
Hesamheidari <74621490+Hesamheidari@users.noreply.github.com>
2023-04-11 01:36:14 +0330

    Update Display_profile

--- vakilpors_Frontend/src/components/profile/Display_Profile.jsx
+++ vakilpors_Frontend/src/components/profile/Display_Profile.jsx
@@ -4,27 +4,28 @@ import ReactDOM from 'react-dom';
 import ProfileDisplay from './ProfileDisplay';
 import ProfileEdit from './ProfileEdit';
 
-const Main = () => {
+const Display_Profile = () => {
   const [username, setUsername] = useState('Hesam');
   const [email, setEmail] = useState('Hesam@example.com');
+  const [job, setJob] = useState('lawyer')
   const [bio, setBio] = useState('I love coding!');
   const [imageURL, setImageURL] = useState('https://1fid.com/girls-profile-pics/');
 
   const handleSave = (newData) => {
     setUsername(newData.username);
     setEmail(newData.email);
+    setJob(newData.job);
     setBio(newData.bio);
     setImageURL(newData.imageURL);
   };
 
   return (
     <div>
-      <ProfileDisplay username={username} email={email} bio={bio} imageURL={imageURL} />
-      <ProfileEdit initialUsername={username} initialEmail={email} initialBio={bio} initialImageURL={imageURL} onSave={handleSave} />
+      <ProfileDisplay username={username} email={email} job={job} bio={bio} imageURL={imageURL} />
     </div>
   );
 };
 
 
 
-export default Main;
+export default Display_Profile;
