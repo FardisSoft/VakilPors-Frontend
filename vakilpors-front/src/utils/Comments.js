6bf06109a899e2f99b0e7fbb67edc1d37011d8d9
AmirReza25 <118440255+AmirReza25@users.noreply.github.com>
2023-04-12 17:18:57 +0330

    Update Comments.js

--- vakilpors-front/src/utils/Comments.js
+++ vakilpors-front/src/utils/Comments.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import '../css/forum.css';
 
 const Comments = ({ numberOfComments, threadId }) => {
 	const navigate = useNavigate();
