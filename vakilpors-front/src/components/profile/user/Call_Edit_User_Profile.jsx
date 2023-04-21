de07ae4cd945fe6bc82ccd2c4a88bdfc02d32e2f
Moosa-Salehi <moosa.salehi.pe@gmail.com>
2023-04-21 20:31:07 +0430

    user profile avatar completed
    and some minor improvements

--- vakilpors-front/src/components/profile/user/Call_Edit_User_Profile.jsx
+++ vakilpors-front/src/components/profile/user/Call_Edit_User_Profile.jsx
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import useStateRef from 'react-usestateref';
-import TextField from '@mui/material/TextField';
-import { Button, Input } from '@mui/material';
-import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+import { Button } from '@mui/material';
+import { MuiFileInput } from 'mui-file-input'
 import '../css/a.css';
 import { Helmet } from 'react-helmet-async';
 import jwt from 'jwt-decode';
@@ -12,32 +11,18 @@ import { updateUser } from '../../../services/userService';
 
 const Call_Edit_User_Profile = () => {
 
+  const { getAccessToken } = useAuth();
   const descriptionUser = "کاربر گرامی ! در این قسمت می توانید تمامی اطلاعات کاربری خود را بروزرسانی و یا ویرایش کنید. لطفا از صحت اطلاعات وارد شده اطمینان حاصل نمائید.";
-  const [description, setDescription] = useState(descriptionUser);
-  const { refUserRole, getAccessToken } = useAuth();
-
   const [errorMessage, setErrorMessage] = useState("");
   const [errorColor, setErrorColor] = useState("red");
-
-
   const [getdetail, setdetail, refdetail] = useStateRef({});
   
-
-  const handleAvatarChange = (event) => {
-    console.log('oomad v ',refdetail.current);
-    const file = event.target.files[0];
-    if (file) {
-      const reader = new FileReader();
-      reader.readAsDataURL(file);
-      reader.onloadend = () => {
-        setdetail({
-          ...getdetail,
-          ['profileImageUrl']: reader.result,
-        });
-      };
-    }
+  const handleAvatarChange = (file) => {
+    setdetail({
+        ...getdetail,
+        ['profileImageUrl']: file,
+      });
   };
-
 
   useEffect(() => {
     const fetchData = async () => {
@@ -88,14 +73,11 @@ const Call_Edit_User_Profile = () => {
         <div className="form-v4-content-ForEdit">
           <div className="form-left">
             <h2>ویرایش اطلاعات کاربری</h2>
-            <p className="text-1">{description}</p>
+            <p className="text-1">{descriptionUser}</p>
           </div>
-
           <form className="form-detail" id="myform">
             <h2>اطلاعات کاربری</h2>
-
             <div className="form-group">
-
               <div className="form-row form-row-1">
                 <label style={{ position: "relative", top: "5px" }}><p>نام و نام خانوادگی</p></label>
                 <input
@@ -128,7 +110,6 @@ const Call_Edit_User_Profile = () => {
                   name="job"
                   value={getdetail.job}
                   onChange={setUserInfo} />
-
               </div>
               <div className="form-row form-row-1" >
                 <label style={{ position: "relative", top: "5px" }}><p>بیوگرافی</p></label>
@@ -136,44 +117,26 @@ const Call_Edit_User_Profile = () => {
                   className="input100"
                   type="text"
                   name="bio"
-                  // required
                   value={getdetail.bio}
                   onChange={setUserInfo}
                   margin="normal" />
               </div>
             </div>
             <div className="form-group">
-              {/* <div className="form-row form-row-1" >
+              <div className="form-row form-row-1">
                 <label style={{ position: "relative", top: "5px" }}><p>عکس پروفایل</p></label>
-                <input
-                  className="input100"
-                  type="file"
-                  name="profileImageUrl"
-                  style={{justifyContent:'center',alignItems:'center'}}
-                  value={profileImage}
-                  onChange={handleAvatarChange}
-                  margin="normal" />
-              </div> */}
-              {/* <div className="form-row form-row-1">
-                <Button variant="contained" color="primary" component="label" startIcon={<CloudUploadIcon sx={{color:"white"}}/>}>
-                  <p style={{ color: "white", position: "relative", right: "8px" }}>انتخاب عکس پروفایل</p>
-                  <input type="file" hidden id="fileInput" onChange={handleAvatarChange} />
-                </Button>
-                { refdetail.current.profileImageUrl && <Input htmlFor="fileInput" value={refdetail.current.profileImageUrl.name} disabled /> }
-              </div> */}
+                <MuiFileInput fullWidth margin='10px' value={getdetail.profileImageUrl} onChange={handleAvatarChange} />
+              </div>
             </div>
-
             <div className="form-row-last">
-              <Button type="submit" variant="contained" color="primary"  onClick={updateuser}>
+              <Button sx={{marginTop:'20px',fontFamily:'shabnam'}} type="submit" variant="contained" color="primary"  onClick={updateuser}>
                 ثبت اطلاعات
               </Button>
-              <label className="container"><p className="text" style={{color:errorColor}}>{errorMessage}</p></label>
+              <label className="container" style={{marginTop:'20px'}}><p className="text" style={{color:errorColor}}>{errorMessage}</p></label>
             </div>
           </form>
         </div>
       </div>
-
-
     </>
   );
 };
