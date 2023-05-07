ee5f2e260917b3e729507268140b4f870de595ab
Moosa-Salehi <moosa.salehi.pe@gmail.com>
2023-05-07 20:38:22 +0430

    animations added to rate page
    also two buttons

--- vakilpors-front/src/components/Rate.js
+++ vakilpors-front/src/components/Rate.js
@@ -1,11 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from "react-router-dom";
-import { Grid } from '@mui/material';
-import Rating from "@mui/material/Rating";
-import Avatar from "@mui/material/Avatar";
-import TextField from "@mui/material/TextField";
-import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
+import { Grid, Rating, Avatar, TextField, Typography, Slide, Box, Button } from '@mui/material';
 import { Helmet } from 'react-helmet-async';
 import { useParams } from "react-router-dom";
 import { useAuth } from "../context/AuthProvider";
@@ -13,7 +8,13 @@ import { useAuth } from "../context/AuthProvider";
 const Rate = () => {
   const [value, setValue] = useState(0);
   const [comment, setComment] = useState("");
+  const [show, setShow] = useState(false);
+
   const { LawyerId } = useParams();
+
+  useEffect(() => {
+    setShow(true);
+  }, []);
 
   const handleChange = (event) => {
     setValue(event.target.value);
@@ -23,12 +24,21 @@ const Rate = () => {
     setComment(event.target.value);
   };
 
+  const handleRegister = () => {
+    alert(`You have registered your comment and rate: ${comment}, ${value}`);
+  };
+
+  const handleCancel = () => {
+    setValue(0);
+    setComment("");
+  };
+
   return (
     <>
     <Helmet>
       <title>Rate Page</title>
     </Helmet>
-    <Box
+    <Grid
       sx={{
         display: "flex",
         flexDirection: "column",
@@ -36,31 +46,59 @@ const Rate = () => {
         margin: 2,
       }}
     >
-      <Avatar
-        alt="User Name"
-        src="https://i.pravatar.cc/150?img=1"
-        sx={{ width: 56, height: 56 }}
-      />
-      <Typography variant="h6" component="div" sx={{ margin: 1 }}>
-        User Name
-      </Typography>
-      <Rating
-        name="rate-user"
-        value={value}
-        onChange={handleChange}
-        sx={{ margin: 1 }}
-      />
-      <TextField
-        id="comment"
-        label="Comment"
-        multiline
-        rows={4}
-        value={comment}
-        onChange={handleComment}
-        variant="outlined"
-        sx={{ width: "80%" }}
-      />
-    </Box>
+      <Slide in={show} direction="right">
+        <Avatar
+          alt="User Name"
+          src="https://i.pravatar.cc/150?img=1"
+          sx={{ width: 56, height: 56 }}
+        />
+      </Slide>
+      <Slide in={show} direction="right">
+        <Typography variant="h6" component="div" sx={{ margin: 1 }}>
+          User Name
+        </Typography>
+      </Slide>
+      <Slide in={show} direction="left">
+        <Box sx={{ display: "flex", alignItems: "center" }}>
+          <Rating
+            name="rate-user"
+            value={value}
+            onChange={handleChange}
+            sx={{ margin: 1 }}
+          />
+          <Box sx={{ fontSize: "0.8rem", color: "#333" }}>
+            {value ? `${value} star${value !== 1 ? "s" : ""}` : "No rating given"}
+          </Box>
+        </Box>
+      </Slide>
+      <Slide in={show} direction="up">
+        <TextField
+          id="comment"
+          label="Comment"
+          multiline
+          rows={4}
+          value={comment}
+          onChange={handleComment}
+          variant="outlined"
+          sx={{
+            width: "80%",
+            // padding: 1,
+            // border: `1px solid ${theme.palette.common.white}`,
+            // backgroundColor: "#f0f0f0",
+          }}
+        />
+      </Slide>
+      <Slide in={show} direction="up">
+        <Box sx={{ display: "flex", gap: 2, margin: 2 }}>
+          <Button variant="contained" color="primary" onClick={handleRegister}>
+            Register
+          </Button>
+          <Button variant="outlined" color="secondary" onClick={handleCancel}>
+            Cancel
+          </Button>
+        </Box>
+      </Slide>
+    </Grid>
     </>
   );
 };
