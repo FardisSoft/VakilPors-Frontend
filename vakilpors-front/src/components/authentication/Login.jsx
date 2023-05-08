6c532bf8127a54a5a179d3e14d34eccdb34cf261
Moosa-Salehi <moosa.salehi.pe@gmail.com>
2023-05-09 03:07:30 +0430

    login page redesigned
    login css files removed

--- vakilpors-front/src/components/authentication/Login.jsx
+++ vakilpors-front/src/components/authentication/Login.jsx
@@ -1,27 +1,46 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { Helmet } from 'react-helmet-async';
-import { useNavigate } from "react-router-dom";
-import '../../css/login-page-main-style.css';
-import '../../css/login-page-util-style.css';
-import { FaEye } from 'react-icons/fa';
-import showPwdImg from '../../assests/images/show-password.svg';
-import hidePwdImg from '../../assests/images/hide-password.svg';
 import { useAuth } from "../../context/AuthProvider";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { Grid, TextField, Typography, Slide, Button, InputAdornment, IconButton } from '@mui/material';
+import smilinglawyer from '../../assests/images/lawyer_smiler.jpg';
+import { Visibility, VisibilityOff } from "@mui/icons-material";
 
+// mui rtl
+import rtlPlugin from 'stylis-plugin-rtl';
+import { CacheProvider } from '@emotion/react';
+import createCache from '@emotion/cache';
+import { createTheme } from '@mui/material/styles';
+import { ThemeProvider } from '@mui/material/styles';
+const cacheRtl = createCache({
+    key: 'muirtl',
+    stylisPlugins: [rtlPlugin],
+});
+const theme = createTheme({
+    direction: 'rtl',
+});
+// mui rtl
 
 const Login = () => {
 
     const [errorMessage, setErrorMessage] = useState("");
     const [errorColor, setErrorColor] = useState("red");
-    const [isRevealPwd, setIsRevealPwd] = useState(false);
+    
     const [getUser, setUser] = useState({
         phoneNumber: "",
         password: ""
     });
 
+    const [show, setShow] = useState(false);
+    const [showPassword, setShowPassword] = useState(false);
+    const handleClickShowPassword = () => setShowPassword(!showPassword);
+    const handleMouseDownPassword = () => setShowPassword(!showPassword);
+
+    useEffect(() => {
+        setShow(true);
+    }, []);
 
     const showErrorMessage = (errorMessage) => {
         toast.error(errorMessage, {
@@ -36,11 +55,10 @@ const Login = () => {
             rtl:true,
             });
     };
-
     const showSuccesMessage = (payam) => {
         toast.success(payam, {
             position: "bottom-right",
-            autoClose: 5000,
+            autoClose: 1000,
             hideProgressBar: false,
             closeOnClick: true,
             pauseOnHover: true,
@@ -73,8 +91,6 @@ const Login = () => {
             else{
                 showErrorMessage("ورود با خطا مواجه شد.");
             }
-            // console.log(localStorage.getItem('accessToken'),"\n refresh : ", localStorage.getItem('refreshToken'));
-            // console.log("main role : ", refUserRole.current);
         }
     };
 
@@ -94,75 +110,108 @@ const Login = () => {
         <Helmet>
             <title>Login</title>
         </Helmet>
-        <div className="limiter">
-            <div className="container-login100">
-                <div className="wrap-login100">
-                    <form className="login100-form validate-form" onSubmit={createUser}>
-                    <ToastContainer />
-                        <span className="login100-form-title p-b-26">
-                            به وکیل پرس خوش آمدید!
-                        </span>
-                        <span className="login100-form-title p-b-48">
-                            <i className="zmdi zmdi-font"></i>
-                        </span>
-
-                        <div className="wrap-input100 validate-input" data-validate="">
-                            <h5 className="txt2-bold">شماره موبایل</h5>
-                            <input
-                                className="input100"
-                                type="text"
-                                name="phoneNumber"
-                                value={getUser.phoneNumber}
-                                onChange={setUserInfo} />
-
-                            <label className="focus-input100"></label>
-                        </div>
-
-                        <div className="wrap-input100 validate-input" data-validate="Enter password">
-
-                            <h5 className="txt2-bold">رمز عبور</h5>
-                            <input
-                                className="input100"
-                                type={isRevealPwd ? "text" : "password"}
-                                name="password"
-                                value={getUser.password}
-                                onChange={setUserInfo}
-                            />
-                            <span className="btn-show-pass mx-5 my-2">
-                                <i className="zmdi zmdi-eye mx-5" title={isRevealPwd ? "Hide password" : "Show password"}
-                                    src={isRevealPwd ? hidePwdImg : showPwdImg}
-                                    onClick={() => setIsRevealPwd(prevState => !prevState)}><FaEye /></i>
-                            </span>
-                            <span className="focus-input100"></span>
-                        </div>
-
-                        <div className="container-login100-form-btn text-center" >
-                            <div className="wrap-login100-form-btn" >
-                                <div className="login100-form-bgbtn"></div>
-                                <button type="submit">
-
-                                    <p style={{ color: "white",width : "200px"}}>ورود</p>
-                                </button>
-                            </div>
-                        </div>
-                        <label className="container"><p className="text" style={{ color: errorColor }}>{errorMessage}</p></label>
-                        <div className="text-center p-t-46 p-b-20">
-                            <span className="txt2">
-                                اکانت ندارید؟ <Link to="/Register">ثبت نام</Link> کنید!
-                            </span>
-                            <br></br>
-                            <span className="txt2">
-                                <Link to="/Forgot_Password">فراموشی رمز عبور</Link>
-                            </span>
-                        </div>
-                    </form>
-                </div>
-            </div >
-        </div >
+        <ThemeProvider theme={theme}>
+        <CacheProvider value={cacheRtl}>
+        <Grid container 
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          height: "100vh",
+          backgroundImage:`url(${smilinglawyer})`,
+          backgroundRepeat:'no-repeat',
+          backgroundSize:'cover',
+          backgroundPosition:'center',
+          }}>
+            <Grid 
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              margin: 2,
+              padding: 2,
+              backgroundColor: "rgba(0,0,0,0.1)",
+              borderRadius: '20px',
+              width: {xs:'90%',sm:'60%',md:'40%'}}}>
+                <Slide in={show} direction="up">
+                    <Grid justifyContent={'center'}>
+                        <Typography sx={{ fontFamily:"shabnam", fontSize:"21px", fontWeight:"bold", align:"center",mb:"10px",color:"rgb(25,117,210)" }}>به وکیل پرس خوش آمدید!</Typography>
+                    </Grid>
+                </Slide>
+                <Slide in={show} direction="left">
+                    <TextField
+                      id="phoneNumber"
+                      label="شماره موبایل"
+                      type="text"
+                      name="phoneNumber"
+                      value={getUser.phoneNumber}
+                      onChange={setUserInfo}
+                      variant="outlined"
+                      inputProps={{ dir: "rtl", style: { fontFamily:"shabnam", fontSize: "17px",color:"black",} }}
+                      InputLabelProps={{ align: "right", dir: "rtl", style: { fontFamily:"shabnam", fontSize: "17px",color:"black",} }}
+                      sx={{
+                        width: {xs:'100%',sm:'80%'},
+                        padding: 0,
+                        backgroundColor: 'rgba(255,255,255,0.5)',
+                        mb: '10px',
+                        borderRadius:"5px",
+                      }}/>
+                </Slide>
+                <Slide in={show} direction="right">
+                    <TextField
+                      id="password"
+                      label="رمز عبور"
+                      type={showPassword ? "text" : "password"}
+                      name="password"
+                      value={getUser.password}
+                      onChange={setUserInfo}
+                      variant="outlined"
+                      InputProps={{ dir: "rtl", style: { fontFamily:"shabnam", fontSize: "17px",color:"black",},
+                      endAdornment: (
+                        <InputAdornment position="end">
+                            <IconButton
+                                aria-label="toggle password visibility"
+                                onClick={handleClickShowPassword}
+                                onMouseDown={handleMouseDownPassword}>
+                                {showPassword ? <Visibility /> : <VisibilityOff />}
+                            </IconButton>
+                        </InputAdornment>
+                        )
+                      }}
+                      InputLabelProps={{ align: "right", dir: "rtl", style: { fontFamily:"shabnam", fontSize: "17px",color:"black",} }}
+                      sx={{
+                        width: {xs:'100%',sm:'80%'},
+                        padding: 0,
+                        backgroundColor: 'rgba(255,255,255,0.5)',
+                        borderRadius:"5px",
+                      }}/>
+                </Slide>
+                <Slide in={show} direction="up">
+                    <Grid container direction={{xs:'column',sm:'row'}} sx={{ display: "flex", m: 2, justifyContent:"center" }}>
+                        <Button size={'large'} variant="contained" color="primary" sx={{fontsize:"18px",fontFamily:"shabnam"}} >
+                            ورود
+                        </Button>
+                    </Grid>
+                </Slide>
+                <Slide in={show} direction="up">
+                    <Grid container direction="column" alignItems="center">
+                        <Typography sx={{ color: errorColor }}>{errorMessage}</Typography>
+                        <Typography sx={{ textAlign: "center", fontFamily:"shabnam", align:"center", color:"black" }}>
+                            اکانت ندارید؟ <Link to="/Register">ثبت نام کنید!</Link>
+                        </Typography>
+                        <Typography sx={{ textAlign: "center", fontFamily:"shabnam", align:"center", color:"black" }}>
+                            <Link to="/Forgot_Password">فراموشی رمز عبور</Link>
+                        </Typography>
+                    </Grid>
+                </Slide>
+                <ToastContainer />
+            </Grid>
+        </Grid>
+        </CacheProvider>
+        </ThemeProvider>
         </>
     );
 }
 
 export default Login;
-
-
