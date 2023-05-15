b6e1e8de72bd006aeed068bd29c14db29a7354f2
Moosa-Salehi <moosa.salehi.pe@gmail.com>
2023-05-15 15:57:59 +0430

    Update Footer.js

--- vakilpors-front/src/components/Footer.js
+++ vakilpors-front/src/components/Footer.js
@@ -1,8 +1,6 @@
-import { HomeOutlined, } from "@mui/icons-material";
-import React, { useState } from 'react';
-import { Link, useNavigate } from "react-router-dom";
-import { Box, Grid, Container } from '@mui/material';
-import { Typography } from '@mui/material';
+import React from 'react';
+import { Link } from "react-router-dom";
+import { Box, Grid, Container, Typography } from '@mui/material';
 import { makeStyles } from '@mui/styles';
 import logo from '../assests/images/logoFS.jpg';
 
@@ -38,23 +36,20 @@ const useStyles = makeStyles((theme) => ({
 
 const Footer = () => {
 
-  const navigate = useNavigate();
   const classes = useStyles();
 
   return (
     <footer style={{backgroundColor: 'rgb(25,118,210)'}}>
       <Container maxWidth="lg">
         <Grid py={1} display="flex" flexWrap="wrap" alignItems="center" className={classes.rootBox}>
-          <Link href="#" color="inherit" underline="none">
-            <img src={logo} style={{width:'80px',height:'80px'}}/>
-            Fardis Soft
-          </Link>
+          <img src={logo} style={{width:'80px',height:'80px'}}/>
+          <Typography>Fardis Soft</Typography>
           <Box component="nav" className={classes.footerNav}>
-            <Link href="#" variant="body1" color="textPrimary" className={classes.footerLink}>صفحه اصلی</Link>
-            <Link href="#" variant="body1" color="textPrimary" className={classes.footerLink}>شرایط سایت</Link>
-            <Link href="#" variant="body1" color="textPrimary" className={classes.footerLink}>تماس با ما</Link>
+            <Link to={'/'} className={classes.footerLink}>صفحه اصلی</Link>
+            <Link to={'/Policy'} className={classes.footerLink}>شرایط سایت</Link>
+            <Link to={'/contactUs'} className={classes.footerLink}>تماس با ما</Link>
           </Box>
-          <Typography color="textSecondary" component="p" variant="caption" gutterBottom={false}>تمامی حقوق برای فردیس سافت محفوظ است.</Typography>
+          <Typography fontFamily={'shabnam'} color="textSecondary" component="p" variant="caption" gutterBottom={false}>تمامی حقوق برای Fardis Soft محفوظ است.</Typography>
         </Grid>
       </Container>
     </footer>
