299658472774ba5c734b1ddf28719b935a0bf8c6
AmirReza25 <118440255+AmirReza25@users.noreply.github.com>
2023-05-16 15:52:37 +0330

    Update ContactUs.js
    Adding Google Maps link instead of Google Maps Map.

--- vakilpors-front/src/components/ContactUs.js
+++ vakilpors-front/src/components/ContactUs.js
@@ -18,8 +18,8 @@ const ContactUs = () => {
 					alignItems: 'center',
 					justifyContent: 'center',
 					height: '100vh',
-					backgroundColor: '#ABC0C0'}}>
-					<h1 style={{paddingBottom:15, fontSize:30, color: '#000000', alignSelf:'center'}}>تماس با ما</h1>
+					backgroundColor: '#fffbf5'}}>
+					<h1 style={{paddingBottom:15, fontSize:34, color: '#000000', alignSelf:'center'}}>تماس با ما</h1>
 			<div style={{display: 'flex',
 						flexDirection: 'column',
 						alignItems: 'flex-start',
@@ -27,12 +27,16 @@ const ContactUs = () => {
 						padding: '40px 80px',
 						border: '1px solid blue',
 						backgroundColor: '#fffbf5',
-						borderRadius: '25px',}}>
+						borderRadius: '25px',
+						alignItems: 'center'}}>
 
 				<p style={{fontSize: 15, color: '#444cc6'}}>آدرس : تهران - تهران - دانشگاه علم و صنعت</p>
 				<p style={{fontSize: 15, color: '#444cc6'}}>تلفن گویا : 77240540</p>
 				<p style={{fontSize: 15, color: '#444cc6'}}>ایمیل : pub@iust.ac.ir</p>
-				<p style={{fontSize: 15, color: '#444cc6'}}>کد پستی : 1684613114</p>
+				<p style={{fontSize: 15, color: '#444cc6'}}>کد پستی : 1684613114 <br/><br/></p>
+				<p style={{fontSize: 22, color: '#000000', alignSelf: 'center'}}>مکان</p>
+				<p style={{fontsize: 15, color: '#444cc6'}}> <a href="https://goo.gl/maps/BVieC2q9PMVLP5UV9">نمایش در Google Maps</a></p>
+				
 			</div>
 		</div>
 		</>
