322820d5d014baca622f24d533797519cdd6429f
Moosa-Salehi <moosa.salehi.pe@gmail.com>
2023-05-16 22:48:04 +0430

    Update UserSendCases.jsx

--- vakilpors-front/src/components/case-pages/UserSendCases.jsx
+++ vakilpors-front/src/components/case-pages/UserSendCases.jsx
@@ -1,16 +1,17 @@
 import React, { useState, useEffect } from "react";
 import { Helmet } from 'react-helmet-async';
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import { useAuth } from "../../context/AuthProvider";
 import { BASE_API_ROUTE } from "../../Constants";
 import axios from "axios";
-import { Typography, Grid, TextField } from "@mui/material";
+import { Typography, Grid, TextField, Avatar, Card, CardContent } from "@mui/material";
 
 const UserSendCases = () => {
 
   const [users, setUsers] = useState([]);
   const { LawyerId } = useParams();
   const { getAccessToken } = useAuth();
+  const { navigate } = useNavigate();
 
   useEffect(() => {
     const getUsers = async () => {
@@ -19,7 +20,7 @@ const UserSendCases = () => {
       try{
         const response = await axios.get(url, {headers: {Authorization: `Bearer ${token}`}});
         console.log("success in Getting users : ",response);
-        // setUsers(response.data.data);
+        setUsers(response.data.data);
       }
       catch (error) {
         console.log("error in Getting users : ",error);
@@ -33,8 +34,17 @@ const UserSendCases = () => {
     <Helmet>
       <title>کاربران ارسال کننده پرونده</title>
     </Helmet>
-    <Grid>
-
+    <Grid container display={'flex'} minHeight={'100vh'}>
+      {users.map( (user,index) =>
+        <Grid key={index} container direction={{ xs: 'column', sm: 'row' }}>
+          <Grid onClick={() => navigate(`/show-cases/true_${user.id}`)} display="flex" alignItems={{xs:'center',sm:"flex-start"}} justifyContent={{xs:'center',sm:"flex-start"}} item component={Card}>
+            <CardContent>
+              <Avatar alt="user profile" sx={{ width: 60, height: 60 }} srcSet={user.profileImageUrl} />
+              <Typography sx={{fontFamily:"shabnam"}}>{user.name}</Typography>
+            </CardContent>
+          </Grid>
+        </Grid>
+      )}
     </Grid>
     </>
   );
