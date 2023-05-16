64d97d399c7c3dbc02df20863bc01a22b9623edf
Moosa-Salehi <moosa.salehi.pe@gmail.com>
2023-05-16 22:20:26 +0430

    not complete show lawyers that have access in cards

--- vakilpors-front/src/components/case-pages/ShowCases.jsx
+++ vakilpors-front/src/components/case-pages/ShowCases.jsx
@@ -33,6 +33,20 @@ const ShowCases = () => {
   const navigate = useNavigate();
   const { isLawyer } = useParams();
 
+  const getLawyersThatHaveAccessToDoc = async (docId) => {
+    const token = await getAccessToken();
+    if(token){
+      const url = BASE_API_ROUTE + `Document/GetLawyersThatHaveAccessToDocument?documentId=${docId}`;
+      try {
+        const response = await axios.get(url, {headers: {Authorization: `Bearer ${token}`}});
+        // console.log('response in getLawyersThatHaveAccessToDoc : ',response);
+        return response.data.data;
+      } catch (error) {
+        console.log('error in getLawyersThatHaveAccessToDoc : ',error);
+      }
+    }
+  };
+
   const getCases = async () => {
     const token = await getAccessToken();
     if(token){
@@ -45,6 +59,11 @@ const ShowCases = () => {
       try {
         const response = await (isLawyer == 'true'? axios.post(url, Data, {headers: {Authorization: `Bearer ${token}`}}) : axios.get(url,{headers: {Authorization: `Bearer ${token}`}}));
         setCases(response.data.data);
+        // response.data.data.map(async (casei) => {
+        //   const lawyers = await getLawyersThatHaveAccessToDoc(casei.id);
+        //   // setCases
+        //   console.log(casei.id, lawyers);
+        // });
         // console.log('response in getDocument : ',response);
       } catch (error) {
         console.log('error in getDocument : ',error);
@@ -118,6 +137,10 @@ const ShowCases = () => {
     }
   };
 
+  const showLawyersThatHaveAccessToDoc = (docId) => {
+    return null;
+  };
+
   const card = (casei) => {
     return ( 
     <React.Fragment>
@@ -151,6 +174,7 @@ const ShowCases = () => {
             </a>
           </IconButton>
         </Box>
+        {showLawyersThatHaveAccessToDoc(casei.id)}
       </CardContent>
       {isLawyer == 'false' && 
         <CardActions>
