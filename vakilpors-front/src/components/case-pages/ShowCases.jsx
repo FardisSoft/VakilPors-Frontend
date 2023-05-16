c9aafc0faf9976279ad05d9beb63cd1913af9e3e
Moosa-Salehi <moosa.salehi.pe@gmail.com>
2023-05-16 16:25:28 +0430

    Update ShowCases.jsx

--- vakilpors-front/src/components/case-pages/ShowCases.jsx
+++ vakilpors-front/src/components/case-pages/ShowCases.jsx
@@ -1,133 +1,92 @@
 import React, { useEffect, useState } from "react";
 import { Helmet } from 'react-helmet-async';
 import { Navigate, useNavigate } from "react-router-dom";
-import { useAuth } from "../../context/AuthProvider";
-import Grid from '@mui/material/Grid';
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
-import { Typography } from "@mui/material";
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import { BASE_API_ROUTE } from "../../Constants";
-import axios from "axios";
 import { useParams } from "react-router-dom";
 import useStateRef from 'react-usestateref';
-
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-
+import { useAuth } from "../../context/AuthProvider";
+import { BASE_API_ROUTE } from "../../Constants";
+import axios from "axios";
 import jwt from 'jwt-decode';
-
-
-
+import { Grid, TextField, Button, Typography, Card, CardActions, CardContent } from '@mui/material';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
  
 const ShowCases = () => {
 
-
+  const [Cases, setCases, refCases] = useStateRef([]);
   const { getAccessToken } = useAuth();
   const navigate = useNavigate();
-  
   const { isLawyer } = useParams();
-
-  const [Cases, setCases, refCases] = useStateRef('');
-
-
   
   useEffect(() => {
     const GetCases = async () => {
-  
       const token = await getAccessToken();
       if(token){
         const tokenData = jwt(token);
-        // console.log('isLawyer: ',isLawyer);
         const url = BASE_API_ROUTE + ( isLawyer == 'true' ? `Document/GetDocumentsThatLawyerHasAccessToByUserId` : `Document/GetDocumentsByUserId?userId=${tokenData.uid}`); 
         const Data = {
           "userId": tokenData.uid,
           "lawyerId": 0
         }
-
-        // console.log('url: ',url);
-        // console.log(Data);
-        
-
         try {
-          
           const response = await (isLawyer == 'true'? axios.post(url, Data, {headers: {Authorization: `Bearer ${token}`}}) : axios.get(url,{headers: {Authorization: `Bearer ${token}`}}));
           setCases(response.data.data);
-          // console.log(refCases.current);
           // console.log('response in getDocument : ',response);
         } catch (error) {
-            console.log('error in getDocument : ',error);
+          console.log('error in getDocument : ',error);
         }
-        
       }
     };
     GetCases();
   }, []);
 
-   
-const card = (casei) => {
-  return ( 
-  <React.Fragment>
-    <CardContent>
-      <Typography sx={{fontFamily: "shabnam", fontSize: 14 }} color="text.secondary" gutterBottom>
-        {casei.title}
-      </Typography>
-      <Typography sx={{fontFamily: "shabnam"}} variant="h5" component="div">
-        {casei.caseName}
-      </Typography>
-      <Typography sx={{fontFamily: "shabnam", mb: 1.5 }} color="text.secondary">
-        {casei.documentCategory}
-      </Typography>
-      <Typography sx={{fontFamily: "shabnam"}} variant="body2">
-          
-        حداقل بودجه :{casei.minimumBudget} تومان
-        <br />
-        حداکثر بودجه : {casei.maximumBudget} تومان
-
-      </Typography>
-    </CardContent>
-    <CardActions>
-      <Button onClick={()=> navigate(`/new-case/edit_${casei.id}`)} sx={{fontFamily: "shabnam"}} size="small">ویرایش</Button>
-    </CardActions>
-  </React.Fragment>
-  );
-};
-  
-
-const ClickNewCase = () => {
-  navigate('/new-case/add');
-
-
-
-};
-
-
-    return(
-        <>
-        <Helmet>
-            <title>پرونده های من</title>
-        </Helmet>
-
-
-        <Grid display={"flex"} flexDirection={"column"} margin={"auto"} alignItems={"center"} justifyContent={"center"} width={"100%"} height={"100vh"} backgroundColor={'#ABC0C0'}>
-            <Grid height={"100%"} width={"90%"} borderRadius={"10px"} padding={"10px"} paddingTop={"50px"} paddingX={"50px"} paddingBottom={"50px"} display={"flex"} position={"relative"} m={"2%"} justifyContent={"right"} item xs={4} spacing={5} alignSelf={"center"} backgroundColor={'white'}>        
-              <Grid container direction={"row"}  sx={{ minWidth: 275 }}>
-                {refCases.current && refCases.current.map((casei) => 
-                <Card sx={{mx:"10px" ,height: "300px"}} variant="outlined">{card(casei)}</Card>
-                )}
-              </Grid>
-              <Button onClick={ClickNewCase} sx={{fontFamily: "shabnam"}} variant="contained">افزودن پرونده</Button>
-            </Grid>
-        </Grid>    
-
-        
-        </>
+  const card = (casei) => {
+    return ( 
+    <React.Fragment>
+      <CardContent>
+        <Typography sx={{fontFamily: "shabnam", fontSize: 14 }} color="text.secondary" gutterBottom>
+          {casei.title}
+        </Typography>
+        <Typography sx={{fontFamily: "shabnam"}} variant="h5" component="div">
+          {casei.caseName}
+        </Typography>
+        <Typography sx={{fontFamily: "shabnam", mb: 1.5 }} color="text.secondary">
+          {casei.documentCategory}
+        </Typography>
+        <Typography sx={{fontFamily: "shabnam"}} variant="body2"> 
+          حداقل بودجه :{casei.minimumBudget} تومان
+          <br />
+          حداکثر بودجه : {casei.maximumBudget} تومان
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button onClick={()=> navigate(`/new-case/edit_${casei.id}`)} sx={{fontFamily: "shabnam"}} size="small">ویرایش</Button>
+      </CardActions>
+    </React.Fragment>
     );
+  };
+  
+  const ClickNewCase = () => {
+    navigate('/new-case/add');
+  };
+
+  return(
+      <>
+      <Helmet>
+          <title>پرونده های من</title>
+      </Helmet>
+      <Grid display={"flex"} flexDirection={"column"} margin={"auto"} alignItems={"center"} justifyContent={"center"} width={"100%"} height={"100vh"} backgroundColor={'#ABC0C0'}>
+        <Grid height={"100%"} width={"90%"} borderRadius={"10px"} padding={"10px"} paddingTop={"50px"} paddingX={"50px"} paddingBottom={"50px"} display={"flex"} position={"relative"} m={"2%"} justifyContent={"right"} item xs={4} spacing={5} alignSelf={"center"} backgroundColor={'white'}>        
+          <Grid container direction={"row"}  sx={{ minWidth: 275 }}>
+            {refCases.current && refCases.current.map((casei) => 
+              <Card sx={{mx:"10px" ,height: "300px"}} variant="outlined">{card(casei)}</Card>
+            )}
+          </Grid>
+          <Button onClick={ClickNewCase} sx={{fontFamily: "shabnam"}} variant="contained">افزودن پرونده</Button>
+        </Grid>
+      </Grid>
+      </>
+  );
 }
 
-
-
 export default ShowCases;
-
