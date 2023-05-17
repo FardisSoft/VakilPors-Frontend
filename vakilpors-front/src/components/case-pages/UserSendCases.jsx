610be6b91329d4082b1edbf8695d36362a378f48
Moosa-Salehi <moosa.salehi.pe@gmail.com>
2023-05-17 20:25:50 +0430

    Update UserSendCases.jsx

--- vakilpors-front/src/components/case-pages/UserSendCases.jsx
+++ vakilpors-front/src/components/case-pages/UserSendCases.jsx
@@ -4,7 +4,7 @@ import { useParams, useNavigate } from "react-router-dom";
 import { useAuth } from "../../context/AuthProvider";
 import { BASE_API_ROUTE } from "../../Constants";
 import axios from "axios";
-import { Typography, Grid, Avatar, Card, CardContent } from "@mui/material";
+import { Typography, Grid, Avatar, Card, CardContent, CardHeader } from "@mui/material";
 
 const UserSendCases = () => {
 
@@ -34,19 +34,28 @@ const UserSendCases = () => {
     <Helmet>
       <title>کاربران ارسال کننده پرونده</title>
     </Helmet>
-    <Grid container display={'flex'} minHeight={'100vh'}>
+    <Grid container minHeight={'100vh'} direction='column'>
+      <Grid display="flex" alignItems="flex-start" justifyContent="flex-start" item component={Card}>
+          <CardHeader titleTypographyProps={{ mx:0, mb:0, mt:'20px', fontFamily:"shabnam", fontWeight:"bold", fontSize:"16px", color:"grayText" }} 
+          title={users.length == 0 ? 'هنوز پرونده ای برای شما ارسال نشده است.' : "کاربرانی که برای شما پرونده ارسال کرده اند:"}/>
+      </Grid>
       {users.map( (user,index) =>
         <Grid key={index} container direction={{ xs: 'column', sm: 'row' }}>
-          <Grid onClick={() => navigate(`/show-cases/true_${user.id}_${LawyerId}`)} display="flex" alignItems={{xs:'center',sm:"flex-start"}} justifyContent={{xs:'center',sm:"flex-start"}} item component={Card} sx={{cursor:'pointer'}}>
+          <Grid display="flex" alignItems={{xs:'center',sm:"flex-start"}} justifyContent={{xs:'center',sm:"flex-start"}} item component={Card}>
             <CardContent>
               <Avatar alt="user profile" sx={{ width: 60, height: 60 }} srcSet={user.profileImageUrl} />
+            </CardContent>
+          </Grid>
+          <Grid display="flex" alignItems="flex-start" justifyContent="flex-start" item component={Card} sm>
+            <CardContent>
               <Typography sx={{fontFamily:"shabnam"}}>{user.name}</Typography>
+              <Typography onClick={() => navigate(`/show-cases/true_${user.id}_${LawyerId}`)} sx={{fontFamily:"shabnam", fontSize:'14px', color:'blue', cursor:'pointer'}}>مشاهده پرونده ها</Typography>
             </CardContent>
           </Grid>
         </Grid>
       )}
     </Grid>
-    </>
+    </> 
   );
 }
 
