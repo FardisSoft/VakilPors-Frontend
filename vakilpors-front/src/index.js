8edd1c1e923fbd3caaa7206c3b444db5fad34e3e
Moosa-Salehi <moosa.salehi.pe@gmail.com>
2023-05-18 15:30:03 +0330

    Update index.js

--- vakilpors-front/src/index.js
+++ vakilpors-front/src/index.js
@@ -56,7 +56,7 @@ root.render(
               <Route path="/new-case/:func" element={<Sidebar component={AddNewCase}/>}/>
               <Route path="/show-cases/:isLawyer" element={<Sidebar component={ShowCases}/>}/>
               <Route path="/user-send-cases/:LawyerId" element={<Sidebar component={UserSendCases}/>}/>
-              <Route path="/payment/verify" element={<ResponseTransaction />}/>
+              <Route path="/payment/verify" element={<Sidebar component={ResponseTransaction} />}/>
               <Route path="/Rate/:LawyerId" element={<Sidebar component={Rate}/>}/>
               <Route path="/wallet" element={<Sidebar component={Wallet}/>}/>
               <Route path="*" element={<NotFound/>}/>
