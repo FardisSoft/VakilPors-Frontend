c556e580e8d479bd92ef81a39b05afe914c4fabe
Moosa-Salehi <moosa.salehi.pe@gmail.com>
2023-05-31 12:03:18 +0330

    Update Statistics.js

--- vakilpors-front/src/components/admin-pages/Statistics.js
+++ vakilpors-front/src/components/admin-pages/Statistics.js
@@ -1,17 +1,11 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { Helmet } from 'react-helmet-async';
-import { Map, GoogleApiWrapper } from 'google-maps-react';
-import FmdGoodIcon from '@mui/icons-material/FmdGood'; // location icon
-import PhoneIcon from '@mui/icons-material/Phone'; // Phone icon
-import EmailIcon from '@mui/icons-material/Email'; // Email Icon
-import LocalPostOfficeIcon from '@mui/icons-material/LocalPostOffice'; // Post icon
-import { IconButton } from "@mui/material";
 
-const ContactUs = () => {
+const Statistics = () => {
 	return (
 		<>
 		<Helmet>
-			<title>تماس با ما</title>
+			<title>آمار بازدید سایت</title>
 		</Helmet>
 
 		<div style={{display: 'flex',
@@ -30,12 +24,12 @@ const ContactUs = () => {
 						backgroundColor: '#fffbf5',
 						borderRadius: '25px',
 						alignItems: 'center'}}>
-				<FmdGoodIcon></FmdGoodIcon>
+				
 				<p style={{fontSize: 15, color: '#444cc6'}}>استان تهران - شهر تهران - نارمک - خیابان حیدرخانی - دانشگاه علم و صنعت ایران - دانشکده مهندسی کامپیوتر</p>
 				<p style={{fontsize: 15, color: '#444cc6'}}> <a href="https://goo.gl/maps/BVieC2q9PMVLP5UV9">نمایش در Google Maps</a></p>
-				<PhoneIcon></PhoneIcon>
+				
 				<p style={{fontSize: 15, color: '#444cc6'}}>021-77240540</p>
-				<EmailIcon></EmailIcon>
+				
 				<p style={{fontSize: 15, color: '#444cc6'}}>pub@iust.ac.ir</p>				
 			</div>
 		</div>
@@ -43,4 +37,4 @@ const ContactUs = () => {
 	);
 };
 
-export default ContactUs;
+export default Statistics;
