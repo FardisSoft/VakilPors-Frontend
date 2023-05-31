a7d652ee99a035449ffa2a5caf04c8a47a1ef6b9
Moosa-Salehi <moosa.salehi.pe@gmail.com>
2023-05-31 12:34:50 +0330

    Update Statistics.js

--- vakilpors-front/src/components/admin-pages/Statistics.js
+++ vakilpors-front/src/components/admin-pages/Statistics.js
@@ -6,7 +6,7 @@ import axios from "axios";
 
 const Statistics = () => {
 
-	const [statistics, setStatistics] = useState();
+	const [statistics, setStatistics] = useState(null);
 	const { getAccessToken } = useAuth();
 
 	useEffect(() => {
@@ -16,7 +16,7 @@ const Statistics = () => {
 				const url = BASE_API_ROUTE + 'Statistics/GetStatistics';
 				try {
 					const response = await axios.get(url, {headers: {Authorization: `Bearer ${token}`}});
-					// setStatistics();
+					setStatistics(response.data);
 					console.log('response in getting Statistics : ',response);
 				} catch (error) {
 					console.log('error in getting Statistics : ',error);
@@ -29,16 +29,15 @@ const Statistics = () => {
 	return (
 		<>
 		<Helmet>
-			<title>آمار بازدید سایت</title>
+			<title>آمار سایت</title>
 		</Helmet>
-
 		<div style={{display: 'flex',
 					flexDirection: 'column',
 					alignItems: 'center',
 					justifyContent: 'center',
 					height: '100vh',
 					backgroundColor: '#fffbf5'}}>
-					<h1 style={{paddingBottom:15, fontSize:34, color: '#000000', alignSelf:'center'}}>تماس با ما</h1>
+					<h1 style={{paddingBottom:15, fontSize:24, color: '#000000', alignSelf:'center'}}>آمار سایت</h1>
 			<div style={{display: 'flex',
 						flexDirection: 'column',
 						alignItems: 'flex-start',
@@ -48,13 +47,15 @@ const Statistics = () => {
 						backgroundColor: '#fffbf5',
 						borderRadius: '25px',
 						alignItems: 'center'}}>
-				
-				<p style={{fontSize: 15, color: '#444cc6'}}>استان تهران - شهر تهران - نارمک - خیابان حیدرخانی - دانشگاه علم و صنعت ایران - دانشکده مهندسی کامپیوتر</p>
-				<p style={{fontsize: 15, color: '#444cc6'}}> <a href="https://goo.gl/maps/BVieC2q9PMVLP5UV9">نمایش در Google Maps</a></p>
-				
-				<p style={{fontSize: 15, color: '#444cc6'}}>021-77240540</p>
-				
-				<p style={{fontSize: 15, color: '#444cc6'}}>pub@iust.ac.ir</p>				
+				{statistics && <>
+					<p style={{fontSize: 15, color: '#444cc6'}}>بازدید امروز : {statistics.dailyVisits}</p>
+					<p style={{fontSize: 15, color: '#444cc6'}}>بازدید این ماه : {statistics.monthlyVisits}</p>
+					<p style={{fontSize: 15, color: '#444cc6'}}>بازدید امسال : {statistics.yearlyVisits}</p>
+					<p style={{fontSize: 15, color: '#444cc6'}}>تعداد کاربران : {statistics.usersCount}</p>
+					<p style={{fontSize: 15, color: '#444cc6'}}>تعداد وکلا : {statistics.lawyersCount}</p>
+					<p style={{fontSize: 15, color: '#444cc6'}}>تعداد پرونده ها : {statistics.casesCount}</p>		
+					<p style={{fontSize: 15, color: '#444cc6'}}>تعداد پیام ها : {statistics.messagesCount}</p>
+				</>}
 			</div>
 		</div>
 		</>
