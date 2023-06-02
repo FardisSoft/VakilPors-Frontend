7031ece7550abb830148fe785d734d2aae0b4715
Moosa-Salehi <moosa.salehi.pe@gmail.com>
2023-06-02 16:14:37 +0330

    Update NotFound.js

--- vakilpors-front/src/components/NotFound.js
+++ vakilpors-front/src/components/NotFound.js
@@ -1,17 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Helmet } from 'react-helmet-async';
-import { Grid, Typography } from '@mui/material';
+import { Grid, Typography, Slide } from '@mui/material';
+import lawOnline from '../assests/images/law-online.jpg';
 
 const NotFound = () => {
+
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    setShow(true);
+  }, []);
+
   return (
     <>
     <Helmet>
       <title>404</title>
     </Helmet>
-    <Grid> 
-      <Typography>404</Typography>
-      <br/>
-      <Typography fontFamily={'shabnam'}>صفحه مورد نظر یافت نشد</Typography>
+    <Grid container 
+    sx={{
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    height: "100vh",
+    backgroundImage:`url(${lawOnline})`,
+    backgroundRepeat:'no-repeat',
+    backgroundSize:'cover',
+    backgroundPosition:'center',
+        }}>
+        <Grid 
+        sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        margin: 2,
+        padding: 2,
+        backgroundColor: "white",
+        borderRadius: '20px',
+            width: {xs:'90%',sm:'60%',md:'40%'}}}>
+            <Slide in={show} direction="left">
+                <Grid justifyContent={'center'}>
+                    <Typography sx={{ fontFamily:"shabnam", fontSize:"40px", fontWeight:"bold", align:"center",mb:"10px",color:"rgb(25,117,210)" }}>404</Typography>
+                </Grid>
+            </Slide>
+            <Slide in={show} direction="right">
+                <Grid justifyContent={'center'}>
+                    <Typography sx={{ fontFamily:"shabnam", fontSize:"21px", fontWeight:"bold", align:"center",mb:"10px",color:"rgb(25,117,210)" }}>صفحه مورد نظر یافت نشد</Typography>
+                </Grid>
+            </Slide>
+        </Grid>
     </Grid>
     </>
   );
