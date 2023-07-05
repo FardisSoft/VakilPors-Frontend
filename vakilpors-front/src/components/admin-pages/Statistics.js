a81f4635742a84b89916c9174648249549f08532
Moosa-Salehi <moosa.salehi.pe@gmail.com>
2023-07-05 23:51:49 +0330

    Update Statistics.js

--- vakilpors-front/src/components/admin-pages/Statistics.js
+++ vakilpors-front/src/components/admin-pages/Statistics.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from "react";
+import useStateRef from "react-usestateref";
 import { Helmet } from 'react-helmet-async';
 import { BASE_API_ROUTE } from "../../Constants";
 import { useAuth } from "../../context/AuthProvider";
@@ -6,15 +7,34 @@ import axios from "axios";
 import Paper from '@mui/material/Paper';
 import { Chart,BarSeries,Title,ArgumentAxis,ValueAxis, } from '@devexpress/dx-react-chart-material-ui';
 import { Animation } from '@devexpress/dx-react-chart';
+import Moment, { max } from 'moment-jalaali';
 
 const Statistics = () => {
 
 	const [statistics, setStatistics] = useState(null);
 	const { getAccessToken } = useAuth();
-	// const [ data, setData ] = ;
+	const [ maxView, setMaxView ] = useState();
+	const [ monthsViews, setMonthsViews, refMonthsViews ] = useStateRef([]);
+
+	const backArray = [26, 13, 99, 88, 77, 66, 55, 44, 130, 100, 10, 4];
+
+	const shamsiMonth = Moment().locale("fa").format('jMMMM');
+	const monthIndex = {'Farvardin': 1, 'Ordibehesht': 2, 'Khordad': 3, 'Tir': 4, 'Mordad': 5, 'Shahrivar': 6, 'Mehr': 7, 'Aban': 8, 'Azar': 9, 'Dey': 10, 'Bahman': 11, 'Esfand': 12}
+	const indexMonth = ['فروردین', 'اردیبهشت', 'خرداد', 'تیر', 'مرداد', 'شهریور', 'مهر', 'آبان', 'آذر', 'دی', 'بهمن', 'اسفند'];
 
 	useEffect(() => {
-		const getStatistics = async () => {
+		const getMonthsViews = () => {
+			if(refMonthsViews.current.length == 0){
+				for (let i = monthIndex[shamsiMonth] ; i > 0; i--) {
+					setMonthsViews([{ month: indexMonth[i-1], view: backArray[i-1] }, ...refMonthsViews.current]);
+				}
+				for (let i = 12; i > monthIndex[shamsiMonth]; i--) {
+					setMonthsViews([{ month: indexMonth[i-1], view: backArray[i-1] }, ...refMonthsViews.current]);
+				}
+			}
+			setMaxView(Math.max(...backArray));
+		}
+		const getStatistics = async () => {				  
 			const token = await getAccessToken();
 			if(token){
 				const url = BASE_API_ROUTE + 'Statistics/GetStatistics';
@@ -27,8 +47,44 @@ const Statistics = () => {
 				}
 			}
 		};
+		getMonthsViews();
 		getStatistics();
 	}, []);
+
+	const getPath = (x, width, y, y1, val) => {
+		if(val == 0) return;
+		if(val <= (maxView / 10) ) 
+			return `M ${x} ${y1}
+					L ${x + width } ${y1}
+					L ${x + width / 2} ${y}
+					Z`;
+		return `M ${x} ${y1} 
+				L ${width + x} ${y1}
+				L ${x + width - 15} ${y + 30}
+				L ${width + x} ${y + 40}
+				L ${x + width / 2} ${y}
+				L ${x} ${y + 40}
+				L ${x + 15} ${y + 30} 
+				Z`;
+	}
+
+	const BarWithLabel = ({ arg, barWidth, maxBarWidth, val, startVal, color, value, style, }) => {
+		const width = maxBarWidth * barWidth;
+		return (
+		  <React.Fragment>
+			<path d={getPath(arg - width / 2, width, val, startVal, value)} fill={value > 50 ? 'rgba(255,0,0,0.6)' : value > 30 ? 'rgba(255,127,39,0.7)' : color} style={style} />
+			<Chart.Label
+			  x={arg}
+			  y={(val + startVal) / 2}
+			  dominantBaseline="middle"
+			  textAnchor="middle"
+			  style={{ fill: 'white' }}
+			>
+			  {value}
+			</Chart.Label>
+		  </React.Fragment>
+		);
+	};
 
 	return (
 		<>
@@ -62,19 +118,11 @@ const Statistics = () => {
 				</>}
 			</div>
 			<Paper>
-				<Chart data={[
-								{ year: '1950', population: 2.525 },
-								{ year: '1960', population: 3.018 },
-								{ year: '1970', population: 3.682 },
-								{ year: '1980', population: 4.440 },
-								{ year: '1990', population: 5.310 },
-								{ year: '2000', population: 6.127 },
-								{ year: '2010', population: 6.930 },
-							]}>
+				<Chart dir={'ltr'} width={1000} data={refMonthsViews.current}>
 					<ArgumentAxis />
 					<ValueAxis />
-					<BarSeries valueField="population" argumentField="year" />
-					<Title text="World population" />
+					<BarSeries valueField="view" argumentField="month" pointComponent={BarWithLabel}/>
+					<Title text="بازدید سایت در 12 ماه گذشته" style={{fontFamily:'shabnam'}} />
 					<Animation />
 				</Chart>
 			</Paper>
