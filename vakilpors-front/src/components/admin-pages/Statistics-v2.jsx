4ab25f598d70036e1be847d18f696f351d913f8b
AylinNaebzadeh <a_naeb@comp.iust.ac.ir>
2023-11-02 21:04:14 +0330

    [add] : some charts to statistics pannel with mock data

--- vakilpors-front/src/components/admin-pages/Statistics-v2.jsx
+++ vakilpors-front/src/components/admin-pages/Statistics-v2.jsx
@@ -11,12 +11,22 @@ import { Animation } from '@devexpress/dx-react-chart';
 import Moment from 'moment-jalaali';
 import { AiOutlineAreaChart, AiFillCheckSquare } from "react-icons/ai";
 import AnimatedCounter from "./AnimatedCounter";
-import { Column } from '@ant-design/plots';
-
+import { Column, Line, Pie } from '@ant-design/plots';
+import { Grid, Typography, Slide, Card } from '@mui/material';
+import { Download, Info } from '@material-ui/icons';
+import DownloadIcon from '@mui/icons-material/Download';
+import InfoIcon from '@mui/icons-material/Info';
 
 
 const VisitPannelStatistics = () => {
 	const [ data_1, setData_1 ] = useState([]);
+
+	const [ data_2, setData_2 ] = useState([]);
+	const [ data_3, setData_3 ] = useState([]);
+	const [ data_4, setData_4 ] = useState([]);
+	const [ data_5, setData_5 ] = useState([]);
+	const [ data_6, setData_6 ] = useState([]);
+
   	const [statistics, setStatistics] = useState(null);
 	const { getAccessToken } = useAuth();
 	const [ maxView, setMaxView ] = useState();
@@ -99,6 +109,45 @@ const VisitPannelStatistics = () => {
 		},
 	};
 
+	// Data for line chart
+	const lineData = [
+		{ year: 'شنبه', value: 30 },
+		{ year: 'یکشنبه', value: 40 },
+		{ year: 'دوشنبه', value: 35 },
+		{ year: 'سه‌شنبه', value: 50 },
+		{ year: 'چهارشنبه', value: 49 },
+		{ year: 'پنج‌شنبه', value: 43 },
+		{ year: 'جمعه', value: 70 }	
+	];
+	
+	// Config for line chart
+	const lineConfig = {
+		data: lineData,
+		xField: 'year',
+		yField: 'value',
+		point: {
+		size: 5,
+		shape: 'diamond',
+		},
+	};
+	
+	// Data for pie chart
+	const pieData = [
+		{ type: 'A', value: 27 },
+		{ type: 'B', value: 25 },
+		{ type: 'C', value: 18 },
+		{ type: 'D', value: 15 },
+		{ type: 'E', value: 10 },
+	];
+	
+	// Config for pie chart
+	const pieConfig = {
+		appendPadding: 10,
+		data: pieData,
+		angleField: 'value',
+		colorField: 'type',
+	};
+
 	const getPath = (x, width, y, y1, val) => {
 		if(val == 0) return;
 		if(val <= (maxView / 10) ) 
@@ -171,75 +220,82 @@ const VisitPannelStatistics = () => {
 
 	return (
 		<>
-		<Helmet>
-			<title>آمار سایت</title>
-		</Helmet>
-		<div style={{display: 'flex',
-					flexDirection: 'column',
-					alignItems: 'center',
-					justifyContent: 'center',
-					minHeight: '100vh',
-					backgroundColor: '#fffbf5'}}>
-            
-					<h1 style={{ paddingBottom:15, fontSize:24, color: '#000000', alignSelf:'center', marginTop: "2rem", fontWeight: "bold" }}><AiOutlineAreaChart /> آمار سایت</h1>
+			<Helmet>
+				<title>آمار سایت</title>
+			</Helmet>
 			<div style={{display: 'flex',
 						flexDirection: 'column',
-						alignItems: 'flex-start',
+						alignItems: 'center',
 						justifyContent: 'center',
-						padding: '40px 10%',
-						border: '5px solid #082640',
-						backgroundColor: '#FCF2F1',
-						borderRadius: '25px',
-            marginBottom: '1rem',
-						alignItems: 'center'}}>
+						minHeight: '100vh',
+						backgroundColor: '#fffbf5'}}>
+				
+						<h1 style={{ paddingBottom:15, fontSize:24, color: '#000000', alignSelf:'center', marginTop: "2rem", fontWeight: "bold" }}><AiOutlineAreaChart /> آمار سایت</h1>
+				<div style={{display: 'flex',
+							flexDirection: 'column',
+							alignItems: 'flex-start',
+							justifyContent: 'center',
+							padding: '40px 10%',
+							border: '5px solid #082640',
+							backgroundColor: '#FCF2F1',
+							borderRadius: '25px',
+				marginBottom: '1rem',
+							alignItems: 'center'}}>
 
-				{statistics && 
-          <div style={{ flexDirection: 'row',
-                                        justifyContent: 'center',
-                                        display: 'flex',
-                                        }}>
-            {window.innerWidth <= 400 && 
-              <>
-                <p style={{fontSize: 15, color: '#444cc6' }}>بازدید امروز : {statistics.dailyVisits}</p>
-                <p style={{fontSize: 15, color: '#444cc6', fontWeight: "bold"}}>بازدید این ماه : {statistics.monthlyVisits}</p>
-                <p style={{fontSize: 15, color: '#444cc6', fontWeight: "bold"}}>بازدید امسال : {statistics.yearlyVisits}</p>
-              </>
-            }
-            <p style={{fontSize: 15, color: '#11447D', fontWeight: "bold", marginLeft: "1rem", marginRight: "0.25rem" }}>
-              <AiFillCheckSquare size='1.75rem' style={{ marginLeft: "0.25rem" }} />
-              تعداد کاربران <span style={{ marginLeft: "0.5rem" }}>:</span> 
-              <AnimatedCounter start={0} end={statistics.usersCount} duration={2.5} delay={1000} />
-            </p>
-            <p style={{fontSize: 15, color: '#11447D', fontWeight: "bold", marginLeft: "1rem", marginRight: "0.25rem" }}>
-              <AiFillCheckSquare size='1.75rem' style={{ marginLeft: "0.25rem" }}/>
-              تعداد وکلا <span style={{ marginLeft: "0.5rem" }}>:</span> 
-              <AnimatedCounter start={0} end={statistics.lawyersCount} duration={2.5} delay={1000} />
-            </p>
-            <p style={{fontSize: 15, color: '#11447D', fontWeight: "bold", marginLeft: "1rem", marginRight: "0.25rem" }}>
-              <AiFillCheckSquare size='1.75rem' style={{ marginLeft: "0.25rem" }}/>
-              تعداد پرونده‌ها <span style={{ marginLeft: "0.5rem" }}>:</span>  
-              <AnimatedCounter start={0} end={statistics.casesCount} duration={2.5} delay={1000} />
-              </p>		
-            <p style={{fontSize: 15, color: '#11447D', fontWeight: "bold", marginLeft: "1rem", marginRight: "0.25rem" }}>
-              <AiFillCheckSquare size='1.75rem' style={{ marginLeft: "0.25rem" }}/>
-              تعداد پیام‌ها <span style={{ marginLeft: "0.5rem" }}>:</span>  
-              <AnimatedCounter start={0} end={statistics.messagesCount} duration={2.5} delay={1000} />
-            </p>
-          </div>
-        }
-				{window.innerWidth > 400 && 
-        		<Paper style={{ padding: '2rem'}}>
-					{/* <Chart dir={'ltr'} width={300} data={refTemp.current} >
-						<ArgumentAxis labelComponent={Label} position="top"/>
-						<ValueAxis />
-						<BarSeries valueField="view" argumentField="month" pointComponent={BarWithLabel}/>
-						<Title textComponent={Title} />
-						<Animation />
-					</Chart> */}
-					<Column {...config} />
-				</Paper>}
+					{statistics && 
+			<div style={{ flexDirection: 'row',
+											justifyContent: 'center',
+											display: 'flex',
+											}}>
+				{window.innerWidth <= 400 && 
+				<>
+					<p style={{fontSize: 15, color: '#444cc6' }}>بازدید امروز : {statistics.dailyVisits}</p>
+					<p style={{fontSize: 15, color: '#444cc6', fontWeight: "bold"}}>بازدید این ماه : {statistics.monthlyVisits}</p>
+					<p style={{fontSize: 15, color: '#444cc6', fontWeight: "bold"}}>بازدید امسال : {statistics.yearlyVisits}</p>
+				</>
+				}
+				<p style={{fontSize: 15, color: '#11447D', fontWeight: "bold", marginLeft: "1rem", marginRight: "0.25rem" }}>
+				<AiFillCheckSquare size='1.75rem' style={{ marginLeft: "0.25rem" }} />
+				تعداد کاربران <span style={{ marginLeft: "0.5rem" }}>:</span> 
+				<AnimatedCounter start={0} end={statistics.usersCount} duration={2.5} delay={1000} />
+				</p>
+				<p style={{fontSize: 15, color: '#11447D', fontWeight: "bold", marginLeft: "1rem", marginRight: "0.25rem" }}>
+				<AiFillCheckSquare size='1.75rem' style={{ marginLeft: "0.25rem" }}/>
+				تعداد وکلا <span style={{ marginLeft: "0.5rem" }}>:</span> 
+				<AnimatedCounter start={0} end={statistics.lawyersCount} duration={2.5} delay={1000} />
+				</p>
+				<p style={{fontSize: 15, color: '#11447D', fontWeight: "bold", marginLeft: "1rem", marginRight: "0.25rem" }}>
+				<AiFillCheckSquare size='1.75rem' style={{ marginLeft: "0.25rem" }}/>
+				تعداد پرونده‌ها <span style={{ marginLeft: "0.5rem" }}>:</span>  
+				<AnimatedCounter start={0} end={statistics.casesCount} duration={2.5} delay={1000} />
+				</p>		
+				<p style={{fontSize: 15, color: '#11447D', fontWeight: "bold", marginLeft: "1rem", marginRight: "0.25rem" }}>
+				<AiFillCheckSquare size='1.75rem' style={{ marginLeft: "0.25rem" }}/>
+				تعداد پیام‌ها <span style={{ marginLeft: "0.5rem" }}>:</span>  
+				<AnimatedCounter start={0} end={statistics.messagesCount} duration={2.5} delay={1000} />
+				</p>
 			</div>
-		</div>
+			}
+					{window.innerWidth > 400 && 
+					<Paper style={{ padding: '2rem'}}>
+						<Column {...config} />
+					</Paper>}
+				</div>
+			</div>
+
+			<Grid container spacing={2}>
+				<Grid item xs={12} sm={6}>
+					<Card sx={{ p: 2 }}>
+						<h2>تعداد بازدید در روزهای هفته</h2>
+						<Line {...lineConfig} />
+					</Card>
+				</Grid>
+				<Grid item xs={12} sm={6}>
+					<Card sx={{ p: 2 }}>
+						<Pie {...pieConfig} />
+					</Card>
+				</Grid>
+			</Grid>
 		</>
 	);
 }
