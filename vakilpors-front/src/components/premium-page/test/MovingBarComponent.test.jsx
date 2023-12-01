a1372b806aff1098c020de0875db2ccaaaafee3a
Amirreza vishteh <74289878+amirrezavishteh@users.noreply.github.com>
2023-12-01 09:41:37 +0330

    fix : test moving bar

--- vakilpors-front/src/components/premium-page/test/MovingBarComponent.test.jsx
+++ vakilpors-front/src/components/premium-page/test/MovingBarComponent.test.jsx
@@ -1,52 +1,25 @@
-import React from 'react';
-import { shallow, mount } from 'enzyme';
-import MovingBarComponent from '../MovingBarComponent';
+import { render, fireEvent } from '@testing-library/react';
+import MovingBarComponent from './MovingBarComponent';
 
 describe('MovingBarComponent', () => {
-    // Write a test case for rendering the component
-    test('renders the component with the given text', () => {
-        // Render the component with shallow rendering
-        const wrapper = shallow(<MovingBarComponent fullText="This is a test" />);
-
-        // Assert that the text is present in the component
-        expect(wrapper.find('.textContainer').text()).toBe('This is a test ');
+    it('renders without crashing', () => {
+        const { getByText } = render(<MovingBarComponent fullText="Test Text" />);
+        expect(getByText('Test Text')).toBeInTheDocument();
     });
 
-    // Write a test case for changing the background color
-    test('changes the background color every 2 seconds', () => {
-        // Render the component with full DOM rendering
-        const wrapper = mount(<MovingBarComponent fullText="This is a test" />);
-
-        // Get the initial background color
-        const initialColor = wrapper.find('.CardContent').prop('style').backgroundColor;
-
-        // Wait for 2 seconds
-        jest.advanceTimersByTime(2000);
-
-        // Get the updated background color
-        const updatedColor = wrapper.find('.CardContent').prop('style').backgroundColor;
-
-        // Assert that the background color has changed
-        expect(updatedColor).not.toBe(initialColor);
+    it('changes color on click', () => {
+        const { getByText, rerender } = render(<MovingBarComponent fullText="Test Text" />);
+        const textElement = getByText('Test Text');
+        const initialColor = textElement.style.backgroundColor;
+        fireEvent.click(textElement);
+        rerender(<MovingBarComponent fullText="Test Text" />);
+        expect(textElement.style.backgroundColor).not.toBe(initialColor);
     });
 
-    // Write a test case for navigating to another page
-    test('navigates to the PremiumLawyers page when clicked', () => {
-        // Render the component with shallow rendering
-        const wrapper = shallow(<MovingBarComponent fullText="This is a test" />);
-
-        // Mock the window.location.href property
-        Object.defineProperty(window, 'location', {
-            value: {
-                href: '',
-            },
-            writable: true,
-        });
-
-        // Simulate a click on the component
-        wrapper.find('.container').simulate('click');
-
-        // Assert that the window.location.href has changed to the PremiumLawyers page
+    it('navigates to /PremiumLawyers on click', () => {
+        const { getByText } = render(<MovingBarComponent fullText="Test Text" />);
+        const textElement = getByText('Test Text');
+        fireEvent.click(textElement);
         expect(window.location.href).toBe('/PremiumLawyers');
     });
 });
