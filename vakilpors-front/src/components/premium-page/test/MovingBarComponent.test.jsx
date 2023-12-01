dc3933777e0de24f4832566ceb31c5a361114d3c
Amirreza vishteh <74289878+amirrezavishteh@users.noreply.github.com>
2023-12-01 09:14:54 +0330

    fix: test moving Bar

--- vakilpors-front/src/components/premium-page/test/MovingBarComponent.test.jsx
+++ vakilpors-front/src/components/premium-page/test/MovingBarComponent.test.jsx
@@ -1,3 +1,70 @@
+// Import Enzyme and Jest
+import { shallow, mount } from 'enzyme';
+import React from 'react';
+
+// Import the component to be tested
+import MovingBarComponent from '../MovingBarComponent';
+
+// Mock the setInterval function to simulate time passing
+jest.useFakeTimers();
+
+// Write a test suite for the component
+describe('MovingBarComponent', () => {
+    // Write a test case for rendering the component
+    test('renders the component with the given text', () => {
+        // Render the component with shallow rendering
+        const wrapper = shallow(<MovingBarComponent fullText="This is a test" />);
+
+        // Assert that the text is present in the component
+        expect(wrapper.find(Typography).text()).toBe('This is a test ');
+    });
+
+    // Write a test case for changing the background color
+    test('changes the background color every 2 seconds', () => {
+        // Render the component with full DOM rendering
+        const wrapper = mount(<MovingBarComponent fullText="This is a test" />);
+
+        // Get the initial background color
+        const initialColor = wrapper.find('.textContainer').prop('style').backgroundColor;
+
+        // Move time forward by 2 seconds
+        jest.advanceTimersByTime(2000);
+
+        // Update the component
+        wrapper.update();
+
+        // Get the updated background color
+        const updatedColor = wrapper.find('.textContainer').prop('style').backgroundColor;
+
+        // Assert that the background color has changed
+        expect(updatedColor).not.toBe(initialColor);
+    });
+
+    // Write a test case for navigating to another page
+    test('navigates to the PremiumLawyers page when clicked', () => {
+        // Render the component with shallow rendering
+        const wrapper = shallow(<MovingBarComponent fullText="This is a test" />);
+
+        // Mock the window.location.href property
+        Object.defineProperty(window, 'location', {
+            value: {
+                href: '',
+            },
+            writable: true,
+        });
+
+        // Simulate a click on the component
+        wrapper.find('.container').simulate('click');
+
+        // Assert that the window.location.href has changed to the PremiumLawyers page
+        expect(window.location.href).toBe('/PremiumLawyers');
+    });
+});
+
+// Clean up mock after all tests
+afterAll(() => {
+    jest.useRealTimers();
+});
 
 
 
