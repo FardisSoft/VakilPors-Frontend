9586254fa3d586434f990458667e0ab4a67d6f04
Amirreza vishteh <74289878+amirrezavishteh@users.noreply.github.com>
2023-12-13 21:06:50 +0330

    fix: moving bar

--- vakilpors-front/src/components/premium-page/MovingBar.jsx
+++ vakilpors-front/src/components/premium-page/MovingBar.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { AppBar, Toolbar, Typography, makeStyles } from "@material-ui/core";
-
+import { Link } from "react-router-dom";
 const useStyles = makeStyles((theme) => ({
     bar: {
         backgroundColor: "gray",
