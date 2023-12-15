83b78c9a903a2c2532c9e8c5281a950704cdc815
Amirreza vishteh <74289878+amirrezavishteh@users.noreply.github.com>
2023-12-15 03:51:50 +0330

    fix : test premium-page

--- vakilpors-front/src/components/premium-page/test/PremiumLawyers.test.jsx
+++ vakilpors-front/src/components/premium-page/test/PremiumLawyers.test.jsx
@@ -1,69 +1,103 @@
 import React from 'react';
 import { render, fireEvent, waitFor } from '@testing-library/react';
 import axios from 'axios';
-import PremiumLawyers from './PremiumLawyers';
+import PremiumLawyers from '../PremiumLawyers';
 
+// Mocking axios post method for activating a subscription
 jest.mock('axios');
 
 describe('PremiumLawyers Component', () => {
-    beforeEach(() => {
-        jest.clearAllMocks();
-    });
+    // Mocking useAuth hook
+    jest.mock('../../context/AuthProvider', () => ({
+        useAuth: () => ({
+            refUserRole: { current: 'User' },
+            getAccessToken: jest.fn(() => 'mockedAccessToken'),
+        }),
+    }));
+
+    // Mocking toast.success and toast.error functions from react-toastify
+    jest.mock('react-toastify', () => ({
+        toast: {
+            success: jest.fn(),
+            error: jest.fn(),
+        },
+    }));
 
     it('renders without crashing', () => {
         render(<PremiumLawyers />);
     });
 
-    it('activates subscription on button click', async () => {
-        axios.post.mockResolvedValue({ data: { /* your response data here */ } });
-
+    it('displays header text', () => {
         const { getByText } = render(<PremiumLawyers />);
-
-        fireEvent.click(getByText('انتخاب'));
-
-        await waitFor(() => expect(axios.post).toHaveBeenCalled());
-        // You can add assertions here based on the expected behavior after a successful activation
+        expect(getByText('رشد کسب و کار خود را با تبلیغات هدفمند آنلاین افزایش دهید')).toBeInTheDocument();
+        expect(getByText('بین برنامه های روزانه یا هفتگی انتخاب کنید تا با مشتریان جدید ارتباط برقرار کنید.')).toBeInTheDocument();
+        expect(getByText('هر روز با مشتریان محلی در تماس باشید.')).toBeInTheDocument();
     });
 
-    it('handles subscription activation error', async () => {
-        axios.post.mockRejectedValue(new Error('Activation failed'));
+    // Add more tests for different parts of the PremiumLawyers component
 
-        const { getByText } = render(<PremiumLawyers />);
+    describe('PlanCard Component', () => {
+        it('renders without crashing', () => {
+            const plan = {
+                name: 'برنامه روزانه',
+                price: '$10',
+                period: 'در روز',
+                description: 'Description of the plan.',
+                commonFeatures: ['Feature 1', 'Feature 2'],
+                planType: 'silver',
+            };
+            render(<PlanCard plan={plan} onSelectPlan={() => { }} loading={false} />);
+        });
 
-        fireEvent.click(getByText('انتخاب'));
+        it('displays plan details correctly', () => {
+            const plan = {
+                name: 'برنامه روزانه',
+                price: '$10',
+                period: 'در روز',
+                description: 'Description of the plan.',
+                commonFeatures: ['Feature 1', 'Feature 2'],
+                planType: 'silver',
+            };
+            const { getByText } = render(<PlanCard plan={plan} onSelectPlan={() => { }} loading={false} />);
 
-        await waitFor(() => expect(axios.post).toHaveBeenCalled());
-        // You can add assertions here based on the expected behavior after an activation error
+            expect(getByText('برنامه روزانه')).toBeInTheDocument();
+            expect(getByText('$10')).toBeInTheDocument();
+            expect(getByText('در روز')).toBeInTheDocument();
+            expect(getByText('Description of the plan.')).toBeInTheDocument();
+            expect(getByText('Feature 1')).toBeInTheDocument();
+            expect(getByText('Feature 2')).toBeInTheDocument();
+        });
+
+        it('calls onSelectPlan when the button is clicked', () => {
+            const plan = {
+                name: 'برنامه روزانه',
+                price: '$10',
+                period: 'در روز',
+                description: 'Description of the plan.',
+                commonFeatures: ['Feature 1', 'Feature 2'],
+                planType: 'silver',
+            };
+            const onSelectPlanMock = jest.fn();
+            const { getByText } = render(<PlanCard plan={plan} onSelectPlan={onSelectPlanMock} loading={false} />);
+
+            fireEvent.click(getByText('انتخاب'));
+
+            expect(onSelectPlanMock).toHaveBeenCalledTimes(1);
+        });
+
+        it('disables the button when loading is true', () => {
+            const plan = {
+                name: 'برنامه روزانه',
+                price: '$10',
+                period: 'در روز',
+                description: 'Description of the plan.',
+                commonFeatures: ['Feature 1', 'Feature 2'],
+                planType: 'silver',
+            };
+            const { getByText } = render(<PlanCard plan={plan} onSelectPlan={() => { }} loading={true} />);
+
+            const button = getByText('انتخاب');
+            expect(button).toBeDisabled();
+        });
     });
-
-    it('displays loading spinner during activation', async () => {
-        axios.post.mockResolvedValue({ data: { /* your response data here */ } });
-
-        const { getByText, getByTestId } = render(<PremiumLawyers />);
-
-        fireEvent.click(getByText('انتخاب'));
-
-        expect(getByTestId('loading-spinner')).toBeInTheDocument();
-
-        await waitFor(() => expect(axios.post).toHaveBeenCalled());
-
-        // You can add assertions here based on the expected behavior after activation
-        expect(getByTestId('loading-spinner')).not.toBeInTheDocument();
-    });
-
-    it('redirects on successful activation', async () => {
-        axios.post.mockResolvedValue({ data: { /* your response data here */ } });
-
-        const { getByText, findByTestId } = render(<PremiumLawyers />);
-
-        fireEvent.click(getByText('انتخاب'));
-
-        await waitFor(() => expect(axios.post).toHaveBeenCalled());
-
-        // Assuming your navigation logic sets a data-testid on the redirected element
-        const redirectedElement = await findByTestId('redirected-element');
-        expect(redirectedElement).toBeInTheDocument();
-    });
-
-    // Add more tests as needed for different scenarios and edge cases
 });
